9b93ab8d3c44fb9f3fda6b6894ca7597559d8f02
Dennis Jakobi <dennis.jakobi76@gmail.com>
2025-12-17 14:11:06 +0100

    Add Operation interface and Calculator class stub
    Added Operation interface with performOperation method. Introduced Calculator class implementing this interface with a not-yet-implemented method. Updated solution to include .gitignore in a new project items folder.

---
+++ Test Lambda Calc/Calculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test_Lambda_Calc
+{
+    internal class Calculator : Operation
+    {
+        public void performOperation(double x, double y)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Test Lambda Calc/Operation.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test_Lambda_Calc
+{
+    internal interface Operation
+    {
+        void performOperation(double x, double y);
+    }
+}
